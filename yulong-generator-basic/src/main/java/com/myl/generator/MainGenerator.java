b1580039287a982a855b73284f0f9dd6e6a7003e
yulong Ma <1817700344@qq.com>
2025-02-16 20:28:57 +0800

    修改了文件路径

--- yulong-generator-basic/src/main/java/com/myl/generator/MainGenerator.java
+++ yulong-generator-basic/src/main/java/com/myl/generator/MainGenerator.java
@@ -25,7 +25,7 @@ public class MainGenerator {
         // 整个项目的根路径
         File parentFile = new File(projectPath).getParentFile();
         // 输入路径
-        String inputPath = new File(parentFile, "yuzi-generator-demo-projects/acm-template").getAbsolutePath();
+        String inputPath = new File(parentFile, "yulong-generator-demo-projects/acm-template").getAbsolutePath();
         String outputPath = projectPath;
         // 生成静态文件
         StaticGenerator.copyFilesByRecursive(inputPath, outputPath);
