f10fa6c3a0fc7af8ef0852c963ab68ede23f5121
yulong Ma <1817700344@qq.com>
2025-02-16 20:00:58 +0800

    修改了doGenerator方法

--- yulong-generator-basic/src/main/java/com/myl/generator/MainGenerator.java
+++ yulong-generator-basic/src/main/java/com/myl/generator/MainGenerator.java
@@ -1,31 +1,46 @@
 package com.myl.generator;
 
+import com.myl.generator.DynamicGenerator;
+import com.myl.generator.StaticGenerator;
 import com.myl.module.MainTemplateConfig;
 import freemarker.template.TemplateException;
 
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * 核心生成器
+ */
 public class MainGenerator {
-    public static void main(String[] args) throws TemplateException, IOException {
-        //1.静态文件生成(先将整个目录复制过来)
-        //获取项目路径
-        String projectPath=System.getProperty("user.dir");
-        //输入路径
-        String inputPath=projectPath+ File.separator+"yulong-generator-demo-projects"+File.separator+"acm-template";
-        //输出路径
-        String outputPath=projectPath;
-        System.out.println(outputPath);
-        //开始复制
-        StaticGenerator.copyFilesByHutool(inputPath,outputPath);
 
-        //2.动态文件生成
-        String DynamicinputPath = projectPath + File.separator + "yulong-generator-basic"+File.separator+"src/main/resources/templates/MainTemplate.java.ftl";
-        String DynamicoutputPath = projectPath + File.separator + "acm-template/src/com/yupi/acm/MainTemplate.java";
+    /**
+     * 生成
+     *
+     * @param model 数据模型
+     * @throws TemplateException
+     * @throws IOException
+     */
+    public static void doGenerate(Object model) throws TemplateException, IOException {
+        String projectPath = System.getProperty("user.dir");
+        // 整个项目的根路径
+        File parentFile = new File(projectPath).getParentFile();
+        // 输入路径
+        String inputPath = new File(parentFile, "yuzi-generator-demo-projects/acm-template").getAbsolutePath();
+        String outputPath = projectPath;
+        // 生成静态文件
+        StaticGenerator.copyFilesByRecursive(inputPath, outputPath);
+        // 生成动态文件
+        String inputDynamicFilePath = projectPath + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
+        String outputDynamicFilePath = outputPath + File.separator + "acm-template/src/com/yupi/acm/MainTemplate.java";
+        DynamicGenerator.doGenerate(inputDynamicFilePath, outputDynamicFilePath, model);
+    }
+
+    public static void main(String[] args) throws TemplateException, IOException {
         MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
         mainTemplateConfig.setAuthor("yupi");
         mainTemplateConfig.setLoop(false);
         mainTemplateConfig.setOutputText("求和结果：");
-        DynamicGenerator.doGenerate(DynamicinputPath, DynamicoutputPath, mainTemplateConfig);
+        doGenerate(mainTemplateConfig);
     }
 }
+
